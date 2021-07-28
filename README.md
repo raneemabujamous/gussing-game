b28f05619e8046d613441f75bb2e53d68b304718
raneemabujamous <raneemabujamous1998@gmail.com>
2021-07-28 16:12:25 +0300

    aded

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # gussing-game
+## in lab 03 
+** i jist try to do page with quiz abouy my self using `for ` `if` and added css file to it  **
+## in lab 04 
+** i complete what i was do in yhe lab before with more javascript code using `function`
+ and work in pairs with my colleague ** < eslamakram 
+ in one time you should be driver and secound should be  
+ 
--- js/app.js
+++ js/app.js
@@ -1,114 +1,156 @@
-let score =0
-alert ("its time for fun let's play gussing game")
-let yourName = prompt("hello Iam raneem nice to meet you .. what's your name?").toLowerCase() ;
-alert("hello " + yourName + " nice to meet you " );
+let score = 0
+alert("its time for fun let's play gussing game")
+let yourName = prompt("hello Iam raneem nice to meet you .. what's your name?").toLowerCase();
+alert("hello " + yourName + " nice to meet you ");
 //console.log("hello",yourName);
- let conf = confirm("let us playing guss ?");
+let conf = confirm("let us playing guss ?");
 
-if (conf == true){ 
-    let personal= prompt("do you think Iam 22 old? please answer ('y' for yes 'n' for no)");
-    while (personal.toLocaleLowerCase() !== "y" && personal.toLocaleLowerCase() !== "n" &&  !personal ){
-        personal= prompt("do you think Iam 22 old? please answer ('y' for yes 'n' for no)");
-    }
-    if(personal == 'y'){
-        score = score+1
-    }else{
-       score = score
-    }
-    alert("I am 22 years old ");
-    //console.log("I am 22 years old");
-let hobbies = prompt("do you think i have a hobbies? please answer ('y'for yes 'n' for no)")
-while(hobbies.toLocaleLowerCase() !== "y" && hobbies.toLocaleLowerCase() !== "n" &&  !hobbies){
-    hobbies= prompt("do you think i have a hobbies? please answer ('y'for yes 'n' for no)");
-}
-if(hobbies == 'y'){
-    score = score+1
-}else{
-   score = score
-}
-alert("it's not hobby exactly but i love sport and i wish travel around the world ")
-//console.log("it's not hobby exactly but i love sport and i wish travel around the world")
-let food = prompt ("do you think i love food? please answer ('y'for yes 'n' for no)")
-while(food !== "y" && food !== "n" &&  !food){
-    food= prompt("do you think i love food? please answer ('y'for yes 'n' for no)").toLocaleLowerCase();
-}
-if(food == 'y'){
-    score = score+1
-}else{
-   score = score
-}
-alert("ohhhhhh so much")
-//console.log("ohhhhhh so much")
-let meet = prompt("do you think i am very happy to meet you ? please answer ('y'for yes 'n' for no)")
-while(meet != "y" && meet != "n" &&  !meet){
-    meet= prompt("do you think i am very happy to meet you ? please answer ('y'for yes 'n' for no)").toLocaleLowerCase();
-}
-if(meet == 'y'){
-    score = score+1
-}else{
-   score = score
-}
-alert("ohh yes sure  you make my day ")
-//console.log("ohh yes sure  you make my day")
+if (conf == true) {
+    function age() {
+        let personal = prompt("do you think Iam 22 old? please answer ('y' for yes 'n' for no)");
 
-
-let gussNum= prompt(" guss the number in my mind ") 
-
-for (let i = 1 ; i<4 ; i++){
-if (gussNum == 12){
-    alert("yes the num is 12")
-    score = score + 1
-    i = 4
-    break;
-}else{
-    if(gussNum<12){
-        alert("too low")
-        gussNum = prompt("now its time to playing guss the number in my mind ")
-    }else{
-        alert("too hight")
-        gussNum = prompt("now its time to playing guss the number in my mind ")
-    }
-}
-    
-}
-console.log(score)
- let gusscountry = prompt("guss my My favorite place to travel؟")
- let place = ["maldives","Germany" ,"Indonesia", "Italy" ]
- for (let i = 1 ; i<6 ; i++){
-    if (gusscountry == place[0] || gusscountry == place[1] || gusscountry == place[2] || gusscountry == place[3]){
-        alert("yes the "+ gusscountry + " is my favorite country")
-        score = score + 1
-        i = 6
-        break;
-    }else{
-        gusscountry = prompt("guss my My favorite place to travel؟")
+        while (personal.toLocaleLowerCase() !== "y" && personal.toLocaleLowerCase() !== "n") {
+            personal = prompt("do you think Iam 22 old? please answer ('y' for yes 'n' for no)");
         }
+        if (personal.toLocaleLowerCase() == 'y') {
+            score = score + 1
+            alert("correct answer")
+        } else {
+            score = score
+            alert("wrong answer yes iam 22 ")
+        }
+        return personal;
     }
-}else{
+    age()
+    //console.log("I am 22 years old");
+    function myHobbies() {
+        let hobbies = prompt("do you think i have a hobbies? please answer ('y'for yes 'n' for no)")
+        console.log(hobbies.toLocaleLowerCase())
+        while (hobbies.toLocaleLowerCase() !== "y" && hobbies.toLocaleLowerCase() !== "n") {
+            hobbies = prompt("do you think i have a hobbies? please answer ('y'for yes 'n' for no)");
+        }
+        if (hobbies == 'y') {
+            score = score + 1
+            alert("correct answer")
+
+        } else {
+            score = score
+            alert("wrong answer yes ihave hobbies")
+
+        }
+        return hobbies;
+    }
+    myHobbies();
+
+    //console.log("it's not hobby exactly but i love sport and i wish travel around the world")
+
+    function loveFood() {
+        let food = prompt("do you think i love food? please answer ('y'for yes 'n' for no)")
+        while (food.toLocaleLowerCase() !== "y" && food.toLocaleLowerCase() !== "n") {
+            food = prompt("do you think i love food? please answer ('y'for yes 'n' for no)");
+        }
+        if (food == 'y') {
+            score = score + 1
+            alert("correct answer")
+
+        } else {
+            score = score
+            alert("wrong answer yes i love food")
+
+        }
+        //console.log("ohhhhhh so much")
+        return food;
+    }
+    loveFood();
+    function niceMeet() {
+        let meet = prompt("do you think i am very happy to meet you ? please answer ('y'for yes 'n' for no)")
+        while (meet.toLocaleLowerCase() !== "y" && meet.toLocaleLowerCase() !== "n") {
+            meet = prompt("do you think i am very happy to meet you ? please answer ('y'for yes 'n' for no)");
+        }
+        if (meet == 'y') {
+            score = score + 1
+            alert("correct answer")
+
+        } else {
+            score = score
+            alert("wrong answer iam happy to meet you")
+
+        }
+        //console.log("ohh yes sure  you make my day")
+        return meet
+    }
+    niceMeet();
+
+    function guss() {
+        let gussNum = prompt(" guss the number in my mind ")
+
+        for (let i = 1; i <= 4; i++) {
+            if (gussNum == 12) {
+                alert("yes the num is 12")
+                score = score + 1
+                i = 4
+                break;
+            } else {
+                if (gussNum < 12) {
+                    alert("too low")
+                    gussNum = prompt("now its time to playing guss the number in my mind ")
+                } else {
+                    alert("too hight")
+                    gussNum = prompt("now its time to playing guss the number in my mind ")
+                }
+
+            }
+            if (i == 4) {
+                alert("the answer is wrong the right one is 12")
+                break;
+            }
+
+        }
+        return gussNum;
+    }
+    guss();
+
+    function travel() {
+        let gusscountry = prompt("guss my My favorite place to travel؟")
+        let place = ["maldives", "Germany", "Indonesia", "Italy"]
+        for (let i = 1; i < 6; i++) {
+            if (gusscountry == place[0] || gusscountry == place[1] || gusscountry == place[2] || gusscountry == place[3]) {
+                alert("yes the " + gusscountry + " is my favorite country")
+                score = score + 1
+                i = 6
+                break;
+            } else {
+                gusscountry = prompt("guss my My favorite place to travel؟")
+            }
+        }
+        return gusscountry
+    }
+    travel();
+} else {
     alert("playing with me my  friend ")
 }
-    console.log(score)
-alert("your score is" + score )
+//console.log(score +" 1-22 ,2-yes i have hobbies ,3-i love food ,4- yes nice to meet you ,5- 12 , 6-maldives,Germany ,Indonesia, Italy" )
+alert(yourName + " your score is " + score + " the right answer 1-22 ,2-yes i have hobbies ,3-i love food ,4- yes nice to meet you ,5- 12 , 6-maldives,Germany ,Indonesia, Italy")
 
 
-let choose = prompt ("Choose a number ( 1 , 2 ,3 ) to give you advice ")
-while (choose != '1' && choose != '2' && choose != '3'){
-    choose =  prompt ("Choose a number ( 1 , 2 ,3 ) to give you advice") ;
+let choose = prompt("Choose a number ( 1 , 2 ,3 ) to give you advice ")
+while (choose != '1' && choose != '2' && choose != '3') {
+    choose = prompt("Choose a number ( 1 , 2 ,3 ) to give you advice");
 };
 
- switch(choose) {
+switch (choose) {
 
-    case  "1" :
-    alert("However difficult life may seem, there is always something you can do and succeed at");
-   break;
+    case "1":
+        alert("However difficult life may seem, there is always something you can do and succeed at");
+        break;
 
-   case "2" :
-   alert("You’re only here for a short visit. Don’t hurry, don’t worry. And be sure to smell the flowers along the way");
-   break;
+    case "2":
+        alert("You’re only here for a short visit. Don’t hurry, don’t worry. And be sure to smell the flowers along the way");
+        break;
 
-   case "3" :
-   alert("“Genius is one percent inspiration, ninety-nine percent perspiration");
-   break;
+    case "3":
+        alert("“Genius is one percent inspiration, ninety-nine percent perspiration");
+        break;
 
 
 }
